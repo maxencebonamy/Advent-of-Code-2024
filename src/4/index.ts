0eb1a85ee5bb9133ab9a498191c260041dbfff94
Maxence Bonamy <maxencebonamy@gmail.com>
2024-12-04 19:01:36 +0100

    4 - second star

--- src/4/index.ts
+++ src/4/index.ts
@@ -2,69 +2,57 @@ import { sum } from "#/utils/array"
 import { logger } from "#/utils/logger"
 import { readFile } from "#/utils/file"
 
-const reverseStrings = (strings: string[]): string[] => {
-	return strings.map(string => string.split("").reverse().join(""))
-}
+const countPatternInLines = (lines: string[], pattern: string[]): number => {
+	const patternWidth = pattern[0].length
+	const patternHeight = pattern.length
+	const linesWidth = lines[0].length
+	const linesHeight = lines.length
 
-const extractLists = (lines: string[]): string[][] => {
-	const lists: string[][] = []
-
-	lists.push(lines)
-	lists.push(reverseStrings(lines))
-
-	const columns = lines.map((line, i) => line.split("").map((_, j) => lines[j][i]).join(""))
-	lists.push(columns)
-	lists.push(reverseStrings(columns))
-
-	const diagonals: string[] = []
-	for (let i = 0; i < lines.length; i++) {
-		let diagonal = ""
-		for (let j = 0; j <= i; j++) {
-			diagonal += lines[j][i - j]
-		}
-		diagonals.push(diagonal)
-	}
-	for (let i = 1; i < lines.length; i++) {
-		let diagonal = ""
-		for (let j = i; j < lines.length; j++) {
-			diagonal += lines[j][lines.length - 1 + i - j]
-		}
-		diagonals.push(diagonal)
-	}
-	for (let i = 0; i < lines.length; i++) {
-		let diagonal = ""
-		for (let j = 0; j <= i; j++) {
-			diagonal += lines[lines.length - 1 - j][i - j]
-		}
-		diagonals.push(diagonal)
-	}
-	for (let i = 1; i < lines.length; i++) {
-		let diagonal = ""
-		for (let j = i; j < lines.length; j++) {
-			diagonal += lines[lines.length - 1 - j][lines.length - 1 + i - j]
-		}
-		diagonals.push(diagonal)
-	}
-	lists.push(diagonals)
-	lists.push(reverseStrings(diagonals))
-
-	return lists
-}
-
-const countWord = (list: string[], regex: RegExp): number => {
 	let matches = 0
 
-	list.forEach(line => {
-		const match = line.match(regex)
-		if (match) {
-			matches += match.length
+	logger.debug("Pattern", pattern)
+
+	for (let i = 0; i < linesHeight - patternHeight + 1; i++) {
+		for (let j = 0; j < linesWidth - patternWidth + 1; j++) {
+			let match = true
+			for (let k = 0; k < patternHeight; k++) {
+				for (let l = 0; l < patternWidth; l++) {
+					if (pattern[k][l] !== "*" && pattern[k][l] !== lines[i + k][j + l]) {
+						match = false
+						break
+					}
+				}
+				if (!match) break
+			}
+			if (match) matches++
 		}
-	})
+	}
+
+	logger.debug("Matches", matches)
 
 	return matches
 }
 
 const lines = readFile("4/input.txt")
-const lists = extractLists(lines)
 
-logger.info(`First solution: ${sum(lists.map(list => countWord(list, /XMAS/g)))}`)
+const first_patterns = [
+	[ "XMAS" ],
+	[ "SAMX" ],
+	[ "X", "M", "A", "S" ],
+	[ "S", "A", "M", "X" ],
+	[ "X***", "*M**", "**A*", "***S" ],
+	[ "***S", "**A*", "*M**", "X***" ],
+	[ "S***", "*A**", "**M*", "***X" ],
+	[ "***X", "**M*", "*A**", "S***" ]
+]
+const first_matches = sum(first_patterns.map(pattern => countPatternInLines(lines, pattern)))
+logger.info(`First solution: ${first_matches}`)
+
+const second_patterns = [
+	[ "M*M", "*A*", "S*S" ],
+	[ "S*M", "*A*", "S*M" ],
+	[ "S*S", "*A*", "M*M" ],
+	[ "M*S", "*A*", "M*S" ]
+]
+const second_matches = sum(second_patterns.map(pattern => countPatternInLines(lines, pattern)))
+logger.info(`Second solution: ${second_matches}`)
