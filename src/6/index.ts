578bad484031b12cbb2cc9c2c95daace3cc445b1
Maxence Bonamy <maxencebonamy@gmail.com>
2024-12-07 14:33:52 +0100

    6 - second solution

--- src/6/index.ts
+++ src/6/index.ts
@@ -4,12 +4,12 @@ import { logger } from "#/utils/logger"
 
 const directions = [ [ 0, -1 ], [ 1, 0 ], [ 0, 1 ], [ -1, 0 ] ]
 
-const getGuardPosition = (map: string[]): [ number, number ] => {
+const getGuardPosition = (map: string[]): [number, number] => {
 	for (let y = 0; y < map.length; y++) {
-		for (let x = 0; x < map[y].length; x++) {
-			if (map[y][x] === "^") return [ x, y ]
-		}
+		const x = map[y].indexOf("^")
+		if (x !== -1) return [ x, y ]
 	}
+	throw new Error("Guard not found")
 }
 
 const isGuardAtBorder = (map: string[]): boolean => {
@@ -17,34 +17,72 @@ const isGuardAtBorder = (map: string[]): boolean => {
 	return position[0] === 0 || position[0] === map[0].length - 1 || position[1] === 0 || position[1] === map.length - 1
 }
 
-const forwardGuard = (map: string[], direction: number): { newMap: string[], newDirection: number } => {
+const forwardGuard = (map: string[], direction: number): { newDirection: number } => {
 	const position = getGuardPosition(map)
-
 	const [ dx, dy ] = directions[direction]
 	const newPosition = [ position[0] + dx, position[1] + dy ]
+
 	if (map[newPosition[1]][newPosition[0]] === "#") {
-		return { newMap: map, newDirection: (direction + 1) % 4 }
+		return { newDirection: (direction + 1) % 4 }
 	}
 
-	const newMap = map.map((line, y) => {
-		return line.split("").map((char, x) => {
-			if (x === newPosition[0] && y === newPosition[1]) return "^"
-			if (x === position[0] && y === position[1]) return "X"
-			return char
-		}).join("")
-	})
-	return { newMap, newDirection: direction }
+	map[position[1]] = map[position[1]].substring(0, position[0]) + "X" + map[position[1]].substring(position[0] + 1)
+	map[newPosition[1]] = map[newPosition[1]].substring(0, newPosition[0]) + "^" + map[newPosition[1]].substring(newPosition[0] + 1)
+
+	return { newDirection: direction }
 }
 
 const countSteps = (map: string[]): number => {
 	return sum(map.map(line => line.split("").filter(char => char === "X" || char === "^").length))
 }
 
-let map = readFile("6/input.txt")
+const addObstacle = (map: string[], position: [number, number]): void => {
+	const [ x, y ] = position
+	map[y] = map[y].substring(0, x) + "#" + map[y].substring(x + 1)
+}
+
+const removeObstacle = (map: string[], position: [number, number]): void => {
+	const [ x, y ] = position
+	map[y] = map[y].substring(0, x) + "." + map[y].substring(x + 1)
+}
+
+const isObstacleValid = (map: string[], position: [number, number]): boolean => {
+	const [ x, y ] = position
+	if (map[y][x] !== "." && map[y][x] !== "X") return false
+
+	const maxIterations = map.length * map[0].length
+	addObstacle(map, position)
+	let currentDirection = 0
+	let steps = 0
+	while (!isGuardAtBorder(map)) {
+		const { newDirection } = forwardGuard(map, currentDirection)
+		currentDirection = newDirection
+		steps++
+		if (steps >= maxIterations) {
+			removeObstacle(map, position)
+			return true
+		}
+	}
+	removeObstacle(map, position)
+	return false
+}
+
+const map = readFile("6/input.txt")
+
+const firstMap = [ ...map ]
 let currentDirection = 0
-while (!isGuardAtBorder(map)) {
-	const { newMap, newDirection } = forwardGuard(map, currentDirection)
-	map = newMap
+while (!isGuardAtBorder(firstMap)) {
+	const { newDirection } = forwardGuard(firstMap, currentDirection)
 	currentDirection = newDirection
 }
-logger.info(`First solution: ${countSteps(map)}`)
+logger.info(`First solution: ${countSteps(firstMap)}`)
+
+let possibleObstacles = 0
+for (let y = 0; y < map.length; y++) {
+	for (let x = 0; x < map[y].length; x++) {
+		if (isObstacleValid([ ...map ], [ x, y ])) {
+			possibleObstacles++
+		}
+	}
+}
+logger.info(`Second solution: ${possibleObstacles}`)
