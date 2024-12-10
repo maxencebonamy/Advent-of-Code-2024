d2fc0c08b29d9817639192c1733b917992994557
Maxence Bonamy <maxencebonamy@gmail.com>
2024-12-10 15:28:54 +0100

    10 - first solution

---
+++ src/10/index.ts
@@ -0,0 +1,62 @@
+import { sum } from "#/utils/array"
+import { readFile } from "#/utils/file"
+import { logger } from "#/utils/logger"
+
+type Point = [number, number]
+
+export const parseMap = (lines: string[]): number[][] => {
+	return lines.map(line => line.split("").map(e => parseInt(e)))
+}
+
+export const getConsecutiveNeighbors = (point: Point, map: number[][]): Point[] => {
+	const [ x, y ] = point
+	const value = map[y][x]
+	const neighbors: Point[] = []
+
+	for (let i = -1; i <= 1; i++) {
+		for (let j = -1; j <= 1; j++) {
+			if (Math.abs(i) === Math.abs(j)) continue
+			if (x + j < 0 || y + i < 0 || x + j >= map[0].length || y + i >= map.length) continue
+			if (map[y + i][x + j] === value + 1) {
+				neighbors.push([ x + j, y + i ])
+			}
+		}
+	}
+
+	return neighbors
+}
+
+export const getTotalTrails = (map: number[][]): number => {
+	let trails: Point[][] = []
+	for (let y = 0; y < map.length; y++) {
+		for (let x = 0; x < map[0].length; x++) {
+			if (map[y][x] === 0) {
+				trails.push([ [ x, y ] ])
+			}
+		}
+	}
+
+	for (let i = 1; i <= 9; i++) {
+		const newTrails: Point[][] = []
+		for (const trail of trails) {
+			const newPoints: Point[] = []
+			for (const point of trail) {
+				const neighbors = getConsecutiveNeighbors(point, map)
+				for (const neighbor of neighbors) {
+					if (!newPoints.some(e => e[0] === neighbor[0] && e[1] === neighbor[1])) {
+						newPoints.push(neighbor)
+					}
+				}
+			}
+			newTrails.push(newPoints)
+		}
+		trails = newTrails
+	}
+
+	return sum(trails.map(trail => trail.length))
+}
+
+const lines = readFile("10/input.txt")
+const map = parseMap(lines)
+const totalTrails = getTotalTrails(map)
+logger.info(`First solution: ${totalTrails}`)
