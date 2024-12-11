d0d5087184100bd0a3ff71f14805020266779c75
Maxence Bonamy <maxencebonamy@gmail.com>
2024-12-11 21:02:51 +0100

    11 - second solution

--- src/11/index.ts
+++ src/11/index.ts
@@ -1,3 +1,4 @@
+import { sum } from "#/utils/array"
 import { readFile } from "#/utils/file"
 import { logger } from "#/utils/logger"
 
@@ -34,7 +35,56 @@ const countStonesAfterBlinks = (arrangement: number[], blinks: number): number =
 	return newArrangement.length
 }
 
-const line = readFile("11/input.txt")[0]
-const arrangement = parseArrangement(line)
+const addStoneInOptimisedArrangement = (arrangement: Record<number, number>, stone: number, repeat = 1): void => {
+	if (arrangement[stone]) {
+		arrangement[stone] += repeat
+	} else {
+		arrangement[stone] = repeat
+	}
+}
 
+const parseOptimisedArrangement = (line: string): Record<number, number> => {
+	const arrangement: Record<number, number> = {}
+	const stones = line.split(" ")
+	for (const stone of stones) {
+		addStoneInOptimisedArrangement(arrangement, parseInt(stone))
+	}
+	return arrangement
+}
+
+const optimisedBlink = (arrangement: Record<number, number>): Record<number, number> => {
+	const newArrangement: Record<number, number> = {}
+
+	for (const stone in arrangement) {
+		if (parseInt(stone) === 0) {
+			newArrangement[1] = arrangement[0]
+			continue
+		}
+		if (stone.length % 2 === 0) {
+			const firstHalf = stone.slice(0, stone.length / 2)
+			const secondHalf = stone.slice(stone.length / 2)
+			addStoneInOptimisedArrangement(newArrangement, parseInt(firstHalf), arrangement[parseInt(stone)])
+			addStoneInOptimisedArrangement(newArrangement, parseInt(secondHalf), arrangement[parseInt(stone)])
+			continue
+		}
+		addStoneInOptimisedArrangement(newArrangement, parseInt(stone) * 2024, arrangement[stone])
+	}
+
+	return newArrangement
+}
+
+const countStonesAfterOptimisedBlinks = (arrangement: Record<number, number>, blinks: number): number => {
+	let newArrangement: Record<number, number> = { ...arrangement }
+	for (let i = 0; i < blinks; i++) {
+		newArrangement = optimisedBlink(newArrangement)
+	}
+	return sum(Object.values(newArrangement))
+}
+
+const line = readFile("11/input.txt")[0]
+
+const arrangement = parseArrangement(line)
 logger.info(`First solution: ${countStonesAfterBlinks(arrangement, 25)}`)
+
+const optimisedArrangement = parseOptimisedArrangement(line)
+logger.info(`Second solution: ${countStonesAfterOptimisedBlinks(optimisedArrangement, 75)}`)
